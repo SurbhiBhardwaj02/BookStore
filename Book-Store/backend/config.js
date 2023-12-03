708d46dcad3000d94fb34349ba1cc7ec0d360ee2
SurbhiBhardwaj <92331117+SurbhiBhardwaj02@users.noreply.github.com>
2023-12-04 00:49:08 +0530

    config.js

--- Book-Store/backend/config.js
+++ Book-Store/backend/config.js
@@ -3,5 +3,3 @@ export const PORT = 5555;
 export const mongoDBURL =
   'mongodb+srv://admin:admin@cluster0.3osw8w6.mongodb.net/books-collection?retryWrites=true&w=majority';
 
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
